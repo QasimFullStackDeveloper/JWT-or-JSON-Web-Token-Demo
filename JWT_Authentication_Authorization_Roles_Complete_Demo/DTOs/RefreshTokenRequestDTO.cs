5ffecf11994bfd80895dcc2bfb2c9b423286d600
Muhammad Qasim Ali <muhammad.bsse4392@iiu.edu.pk>
2025-07-14 14:57:23 +0500

    Implement JWT authentication and user management
    - Updated `.gitattributes` and `.gitignore` for better file handling.
    - Added solution file for JWT authentication project.
    - Created `AuthController` with endpoints for registration, login, and token refresh.
    - Introduced DTO classes for user authentication and token management.
    - Modified `AppDbContext` to include user DbSet.
    - Defined `User` entity with relevant properties.
    - Updated project file to target .NET 9.0 and added necessary package references.
    - Added migration files for database schema changes.
    - Configured services in `Program.cs` for JWT authentication and OpenAPI.
    - Created `AuthService` for authentication logic and defined `IAuthService` interface.
    - Added SQLite database files and updated configuration files for JWT settings.

---
+++ JWT_Authentication_Authorization_Roles_Complete_Demo/DTOs/RefreshTokenRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace JWT_Authentication_Authorization_Roles_RefreshToken_Demo.DTOs
+{
+    public class RefreshTokenRequestDTO
+    {
+        public Guid UserId { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
---
+++ JWT_Authentication_Authorization_Roles_Complete_Demo/DTOs/UserLoginDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JWT_Authentication_Authorization_Roles_RefreshToken_Demo.DTOs
+{
+    public class UserLoginDTO
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public class UserRegisterDTO : UserLoginDTO
+    {
+        public string Role { get; set; } = string.Empty;
+    }
+}
